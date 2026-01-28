3ced7674dc294300f358f9fb9ae87271f3ab7f38
payalkale858 <kalepayal53@gmail.com>
2026-01-28 13:53:28 +0530

    Deploy full portfolio React + Django1

--- porfolio/porfolio/settings.py
+++ porfolio/porfolio/settings.py
@@ -61,13 +61,11 @@ TEMPLATES = [
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [os.path.join(BASE_DIR, "build")],  # React build folder
         "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
+        "OPTIONS": {"context_processors": [
+            "django.template.context_processors.request",
+            "django.contrib.auth.context_processors.auth",
+            "django.contrib.messages.context_processors.messages",
+        ]},
     },
 ]
 
